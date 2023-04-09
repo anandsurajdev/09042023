5520ac6e2a20d76989ca4f90f9fce6a3f1954bea
anandsurajdev <123050966+anandsurajdev@users.noreply.github.com>
2023-04-09 23:35:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function clickevnt(){
+    var fname=document.getElementById("first").value; 
+    var lname=document.getElementById("last").value; 
+    var mname=document.getElementById("middle").value; 
+    var mailid=document.getElementById("mail").value; 
+    console.log(fname);
+    console.log(lname);
+    console.log(mname);
+    console.log(mailid);
+}
